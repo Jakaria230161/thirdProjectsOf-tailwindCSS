22b00b4fd322dbdecd9b320108bf958eefd63cdc
Jakaria230161 <jakariamahmud016@gmail.com>
2023-02-16 21:30:16 +0600

    facebook login page clone

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xxm: '300px',
+      xm: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
